6893091d12c78ada20d911dfd015fe1ceb7d83b0
tbingle <sandmanthomas9@icloud.com>
2025-02-15 18:21:20 -0500

    Task 1- Employee Salary Calculation

--- cc_8.js
+++ cc_8.js
@@ -1,1 +1,14 @@
 //Inital Commit
+//Task 1- Function Declaration
+function calculateSalary(baseSalary, bonus, taxRate) {//declare fucnction
+    let netSalary = (baseSalary + bonus) - (baseSalary * taxRate);//formula
+    return `Net Salary: $${netSalary.toFixed(2)}`;
+}
+let result1 = calculateSalary(5000, 500, 0.1);  //first result
+let result2 = calculateSalary(7000, 1000, 0.15); //second result
+
+console.log(result1);//Log
+console.log(result2);
+
+
+
