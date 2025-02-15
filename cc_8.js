9248491b4c687f53d311d2a718a7df843679c299
tbingle <sandmanthomas9@icloud.com>
2025-02-15 18:28:10 -0500

    Task 2- Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -10,5 +10,14 @@ let result2 = calculateSalary(7000, 1000, 0.15); //second result
 console.log(result1);//Log
 console.log(result2);
 
+//Task 2- Function Expression
+let calculateDiscount = function(price, discountRate) {//declare function
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;//return value 
+};
+let result3 = calculateDiscount(100, 0.2);  // First pricr
+let result4 = calculateDiscount(250, 0.15); //second price
 
+console.log(result3);//log
+console.log(result4);
 
